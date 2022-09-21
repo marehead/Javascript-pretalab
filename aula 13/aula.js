73cec113b45354d991ac93d52d28d785012fb668
mlopezlr <marisantl@hotmail.com>
2022-09-21 11:59:01 -0300

    subindo aula 2

---
+++ aula 13/aula.js
@@ -0,0 +1,45 @@
+const array = [10, 20, 30, 40, 50];
+
+console.log(array)
+
+const alunes = ['', 1, 'Veronica', 2, 'Carolina', 3]
+
+console.log('ARRAY ORIGINAL', alunes)
+
+alunes.push('Drika', 'Vera', 'Paula', 'Nadja')
+console.log('ARRAAY MODIFICADO, alunes)
+
+alunes.pop()
+
+console.log('REMOVENDO VALORES', alunes)
+
+const notas = [1, 2, 3]
+notas.forEach((elemento) => {
+	console.log(nomes)
+})
+
+retornar um os nomes através do ForEach e isNaN
+
+const linguagens = ['JS', 1, 2, 3, 'HTML', 'CSS']
+linguagens.for.Each(linguagem => {
+	if(isNaN(linguagem)) {
+		console.log(linguagem)
+	}
+})
+
+para imprimir um array dentro de outro array
+
+const alunas = ['Thais', 'Veronoca', 'Carolina']
+const cursos = ['JS', 'CSS', 'HTML']
+const pretaLab = [ alunas, cursos]
+console.log(pretaLab[0][1])
+
+
+const cursosLivres = [ 'MinasPrograma'], pretaLab, 'Programaria' ] 
+
+console.log(pretaLab[0][1])
+
+
+
+
+
